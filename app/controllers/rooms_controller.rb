e57fb6136ec92fa1fd72f01401d108e8652c7467
panage-jp <joojoojoo6217@gmail.com>
2019-11-24 23:33:07 +0900

    create message show view

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,4 +1,6 @@
 class RoomsController < ApplicationController
   def index
   end
+  def show
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: "rooms#index"
-  get 'rooms/index'
+  resources :messages
+  resources :rooms
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
