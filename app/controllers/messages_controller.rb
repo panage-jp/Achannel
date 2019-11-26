8d81c0b9842993b54c66b4dfce5222dd85808b10
panage-jp <joojoojoo6217@gmail.com>
2019-11-26 21:29:22 +0900

    ip test

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -40,11 +40,15 @@ class MessagesController < ApplicationController
     client_ip = request.remote_ip
     client_id_num = client_ip.delete(".")
     client_id_num.to_i
-    srand(client_id_num)
-    o = [('a'..'z'), ('A'..'Z'), ('0'..'9')].map { |i| i.to_a }.flatten
-    autho_id = (0...8).map { o[rand(o.length)] }.join
-    return autho_id
-  
+    begin
+      
+      srand(client_id_num)
+      o = [('a'..'z'), ('A'..'Z'), ('0'..'9')].map { |i| i.to_a }.flatten
+      autho_id = (0...8).map { o[rand(o.length)] }.join
+      return autho_id
+    rescue
+      return "???"
+    end
   end
 
     def message_password_authenticate
